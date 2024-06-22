ca692475965cb84f74f2f307b5e07a3097148655
mhadityaps05 <muhamad.sudirman@binus.ac.id>
2024-06-22 17:53:24 +0700

    feat: add home, order, account

--- app/src/main/java/com/example/easypick/FragmentActivity.kt
+++ app/src/main/java/com/example/easypick/FragmentActivity.kt
@@ -2,15 +2,41 @@ package com.example.easypick
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.easypick.databinding.ActivityFragmentBinding
 
 class FragmentActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fragmentReplace(HomeFragment())
+
+
+        binding.buttonNavigation.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home ->fragmentReplace(HomeFragment())
+                R.id.order ->fragmentReplace(OrderFragment())
+                R.id.account ->fragmentReplace(AccountFragment())
+
+                else ->{
+
+                }
+
+            }
+            true
+
+        }
+
+    }
+
+    private fun fragmentReplace(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
     }
 }
